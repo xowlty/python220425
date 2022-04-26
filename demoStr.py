c3605e166306e297fbe862acfa6d16ed5e323cd3
xowlty <thinky1212@gmail.com>
2022-04-26 11:49:32 +0900

    문자열 처리 메소드

---
+++ demoStr.py
@@ -0,0 +1,12 @@
+# demoStr.py
+
+strA = "python is very powerful"
+strB = "파이썬은 강력해"
+print(len(strA))
+print(len(strB))
+print(strA.capitalize())
+print(strA.count("p"))
+print(strA.count("p",7))
+print("MBC2580".isalnum())
+print("MBC:2580".isalnum())
+
--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+#부모 클래스 정의
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,17 +7,26 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
-class Student(Person):
+#자식 클래스 정의
+class Student(Person): #괄호안에는 부모클래스임
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모 클래스 생성자 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    #상속받아서 마음에 안들면 재정의(override)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))  
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))   
+ 
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+
+p.printInfo()
+s.printInfo()
+
+#print(p.__dict__)
+#print(s.__dict__)
 
 
